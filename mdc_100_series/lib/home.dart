6aefa1d17d716bcc8219985771dfe6df302b01cb
hachaniwassim <wassimhach16@gmail.com>
2021-11-14 11:44:20 +0100

    Adding product data and image fitwidth

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,6 +13,11 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,35 +28,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Make a collection of cards (102)
-    List<Card> _buildGridCards(int count) {
-      List<Card> cards = List.generate(
-        count,
-            (int index) => Card(
+// TODO: Make a collection of cards (102)
+
+// Replace this entire method
+    List<Card> _buildGridCards(BuildContext context) {
+      List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+      if (products.isEmpty) {
+        return const <Card>[];
+      }
+
+      final ThemeData theme = Theme.of(context);
+      final NumberFormat formatter = NumberFormat.simpleCurrency(
+          locale: Localizations.localeOf(context).toString());
+
+      return products.map((product) {
+        return Card(
           clipBehavior: Clip.antiAlias,
+          // TODO: Adjust card heights (103)
           child: Column(
+            // TODO: Center items on the card (103)
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               AspectRatio(
-                aspectRatio: 18.0 / 11.0,
-                child: Image.asset('assets/diamond.png'),
+                aspectRatio: 18 / 11,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  // TODO: Adjust the box size (102)
+                  fit: BoxFit.fitWidth,
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Title'),
-                    const SizedBox(height: 8.0),
-                    Text('Secondary Text'),
-                  ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    // TODO: Align labels to the bottom and center (103)
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // TODO: Change innermost Column (103)
+                    children: <Widget>[
+                      // TODO: Handle overflowing labels (103)
+                      Text(
+                        product.name,
+                        style: theme.textTheme.headline6,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
-        ),
-      );
-
-      return cards;
+        );
+      }).toList();
     }
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
@@ -96,11 +130,12 @@ class HomePage extends StatelessWidget {
       body: Center(
         // TODO: Add a grid view (102)
         // TODO: Add a grid view (102)
+// TODO: Add a grid view (102)
         body: GridView.count(
             crossAxisCount: 2,
             padding: const EdgeInsets.all(16.0),
             childAspectRatio: 8.0 / 9.0,
-            children: _buildGridCards(10) // Replace
+            children: _buildGridCards(context) // Changed code
         ),
       ),
       resizeToAvoidBottomInset: false,
